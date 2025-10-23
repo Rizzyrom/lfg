13684b0e9a75bda062de6a48cc166d6715726a94
Rom <rom@lfgmarketpulse.com>
2025-10-22 21:59:15 -0700

    Fix TypeScript error in cache eviction logic
    Add undefined check for oldestKey before deletion to satisfy TypeScript strict null checks

--- lib/cache.ts
+++ lib/cache.ts
@@ -32,7 +32,9 @@ class InMemoryCache {
     // Evict oldest entries if cache is full
     if (this.cache.size >= this.maxSize) {
       const oldestKey = this.cache.keys().next().value
-      this.cache.delete(oldestKey)
+      if (oldestKey !== undefined) {
+        this.cache.delete(oldestKey)
+      }
     }
 
     this.cache.set(key, {
